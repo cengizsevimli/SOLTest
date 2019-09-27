2b897542742814005a1ceeeeeee2034eb4ecb697
cengizsevimli <cengizsevimli1995@gmail.com>
2019-09-27 11:32:43 +0300

    Add files via upload

---
+++ SOLTest/despatch cloud/JS/script.js
@@ -0,0 +1,8 @@
+function showmonthly(){
+    document.getElementById('monthly-pricing').style.display ='block';
+    document.getElementById('yearly-pricing').style.display = 'none';
+  }
+  function showyearly(){
+    document.getElementById('yearly-pricing').style.display = 'block';
+    document.getElementById('monthly-pricing').style.display ='none';
+  }
